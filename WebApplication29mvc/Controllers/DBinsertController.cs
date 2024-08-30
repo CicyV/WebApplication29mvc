9ac2e387448948b06be60b9173fdb4550ecf7b8f
Acer <Acer@DESKTOP-MF4J9K9>
2024-08-30 08:08:57 +0530

    Add project files.

---
+++ WebApplication29mvc/Controllers/DBinsertController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication29mvc.Models;
+
+namespace WebApplication29mvc.Controllers
+{
+    public class DBinsertController : Controller
+    {
+        AugustDBEntities dbobj = new AugustDBEntities();
+        // GET: DBinsert
+        public ActionResult Insert_pageload()
+        {
+            return View();
+        }
+        public ActionResult Insert_click(Class1 clsobj)
+        {
+            if (ModelState.IsValid)
+            {
+            dbobj.sp_stinsert(clsobj.name, clsobj.age, clsobj.address);
+            clsobj.msg = "inserted";
+            return View("Insert_pageload", clsobj);
+             }
+            return View("Insert_pageload", clsobj);
+        }
+    }
+}
---
+++ WebApplication29mvc/Models/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication29mvc.Models
+{
+    public class Class1
+    {
+        [Required]
+        public string name { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+        public string msg { get; set; }
+    }
+}
